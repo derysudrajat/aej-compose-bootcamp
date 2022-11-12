ad81121d1b0a46cd562763df4084bee6ddd925ca
derysudrajat <dery.sudrajat17@gmail.com>
2022-11-12 19:04:20 +0700

    initial commit

---
+++ app/src/main/java/id/derysudrajat/myfirstapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package id.derysudrajat.myfirstapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            // TODO: set content app here
+        }
+    }
+}
